a6f4ee2f7d037f4977b4b86e94c92dfee4a1b22c
Rivveq <111184702+Rivveq@users.noreply.github.com>
2024-05-18 13:16:18 +0200

    Zmiany w oknie aplikacji
    (niedokończone) początek pracy nad dopracowaniem okna aplikacji

--- display.py
+++ display.py
@@ -2,10 +2,10 @@ import customtkinter as customtki
 
 # Logika odpowiedzialna za tworzenie okna aplikacji
 
-customtki.set_appearance_mode("dark")
-customtki.set_default_color_theme("dark-blue")
-
 class App(customtki.CTk):
+
+    customtki.set_appearance_mode("dark")
+    customtki.set_default_color_theme("dark-blue")
 
     def __init__(self):
         super().__init__()
@@ -21,42 +21,55 @@ class App(customtki.CTk):
 
         # Napis tytułowy
         self.label = customtki.CTkLabel(self, text="Konwerter Jednostek", font=customtki.CTkFont(size=30, weight="bold"))
-        self.label.grid(row=0, column=2, columnspan=3, padx=20, pady=(20, 10))
+        self.label.grid(row=0, column=1, columnspan=3, padx=20, pady=(20, 10))
+
+        # Sidebar
+        self.sidebar_frame = customtki.CTkFrame(self, width=180, corner_radius=0)
+        self.sidebar_frame.grid(row=0, column=5, rowspan=4, sticky="nsew")
+        self.sidebar_frame.grid_rowconfigure(4, weight=2)
+
+        # Stworzenie tablicy w sidebarze
+        self.tabview = customtki.CTkTabview(self.sidebar_frame, width=180)
+        self.tabview.grid(row=1, column=5, rowspan=3, padx=10, pady=10)
+        self.tabview.add("Długość")
+        self.tabview.add("Masa")
+        self.tabview.add("Czas")
+        self.tabview.add("Pole powierzchnii")
+        self.tabview.add("Objętość")
+        self.tabview.add("Temperatura")
+        self.tabview.add("Ciśnienie")
+        self.tabview.add("Prędkość")
+        self.tabview.add("Pamięć")
 
         # Podpisy wejscia i wyjscia
         self.labelin = customtki.CTkLabel(self, text="Wejście", font=customtki.CTkFont(size=20, weight="bold"))
-        self.labelin.grid(row=1, column=1, columnspan=2, padx=20, pady=(20, 0))
+        self.labelin.grid(row=1, column=1, padx=20, pady=(10, 0))
 
         self.labelout = customtki.CTkLabel(self, text="Wyjście", font=customtki.CTkFont(size=20, weight="bold"))
-        self.labelout.grid(row=1, column=4, columnspan=2, padx=20, pady=(20, 0))
+        self.labelout.grid(row=1, column=3, padx=20, pady=(10, 0))
 
         # Znak "=" na środku
         self.rowna = customtki.CTkLabel(self, text="=", font=customtki.CTkFont(size=50, weight="bold"))
-        self.rowna.grid(row=2, column=3, padx=10, pady=0)
+        self.rowna.grid(row=2, column=2, padx=10, pady=0)
 
         # Pole wejściowe
         self.entry = customtki.CTkEntry(self, height=55, width=220, placeholder_text="Ilość", fg_color="gray29", corner_radius=8, font=customtki.CTkFont(size=20), justify="center")
-        self.entry.grid(row=2, column=2, padx=5, pady=0)
+        self.entry.grid(row=2, column=1, padx=5, pady=0)
 
         # Pole wyjściowe
         self.output = customtki.CTkLabel(self, height=55, width=220, text="Placeholder", font=customtki.CTkFont(size=20), fg_color="gray29", corner_radius=8)
-        self.output.grid(row=2, column=4, padx=5, pady=0)
+        self.output.grid(row=2, column=3, padx=5, pady=0)
 
-        # Lista jednostek wejścia
-        self.listain = customtki.CTkComboBox(self, width=150,
-                                             values=["ocpja1", "opcja2", "opcja3"])
-        self.listain.grid(row=2, column=1, padx=0, pady=10)
+        # # Lista jednostek wejścia
+        # self.listain = customtki.CTkComboBox(self, width=150,
+        #                                      values=["ocpja1", "opcja2", "opcja3"])
+        # self.listain.grid(row=2, column=1, padx=0, pady=10)
 
-        # Lista jednostek wyjścia
-        self.listain = customtki.CTkComboBox(self, width=150,
-                                             values=["ocpja1", "opcja2", "opcja3"])
-        self.listain.grid(row=2, column=5, padx=0, pady=10)
+        # # Lista jednostek wyjścia
+        # self.listain = customtki.CTkComboBox(self, width=150,
+        #                                      values=["ocpja1", "opcja2", "opcja3"])
+        # self.listain.grid(row=2, column=5, padx=0, pady=10)
         
         # Przycisk do konwersjii
         self.button = customtki.CTkButton(self, height=40, width=180, text="Konwertuj")
-        self.button.grid(row=3, column=2, columnspan= 3)
-
-
-if __name__ == "__main__":
-    app = App()
-    app.mainloop()
+        self.button.grid(row=3, column=1, columnspan=3)
--- konwerter.py
+++ konwerter.py
@@ -1,7 +1,15 @@
 import json
+import display
 
-#Główny plik odpowiedzialny za przeprowadzanie konwersji jednostek
+# Główny plik odpowiedzialny za przeprowadzanie konwersji jednostek
 
-#Otwieranie pliku .json z danymi jednostek
+# Otwieranie pliku .json z danymi jednostek
 with open('jednostki.json') as plik:
     dane_jednostek = json.load(plik)
+
+
+
+# Główny loop wyświetlający okno zaimportowane z oliku dispaly.py
+if __name__ == "__main__":
+    app = display.App()
+    app.mainloop()

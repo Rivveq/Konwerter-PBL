1c41345e184a36922d9a1fd7a87c4fb01003c057
Rivveq <111184702+Rivveq@users.noreply.github.com>
2024-05-30 19:11:27 +0200

    Logika i finalizacja
    Dodanie logika.py z funkcjami do konwersji jednostek oraz podpiecie wszystkich funkcji pod interfejs aplikacji

--- display.py
+++ display.py
@@ -1,4 +1,5 @@
 import customtkinter as customtki
+import logika
 
 # Logika odpowiedzialna za tworzenie okna aplikacji
 
@@ -31,31 +32,31 @@ class App(customtki.CTk):
         self.sidebar_frame.grid_columnconfigure(1, weight=2)
 
         # Przyciski na sidebarze
-        self.sbutton1 = customtki.CTkButton(self.sidebar_frame, height=40, width=120, text="Długość")
+        self.sbutton1 = customtki.CTkButton(self.sidebar_frame, height=40, width=120, text="Długość", command=lambda: self.change_item_list("dlugosc"))
         self.sbutton1.grid(row=1, columnspan=2)
         
-        self.sbutton1 = customtki.CTkButton(self.sidebar_frame, height=40, width=120, text="Masa")
+        self.sbutton1 = customtki.CTkButton(self.sidebar_frame, height=40, width=120, text="Masa", command=lambda: self.change_item_list("masa"))
         self.sbutton1.grid(row=2, columnspan=2)
         
-        self.sbutton1 = customtki.CTkButton(self.sidebar_frame, height=40, width=120, text="Czas")
+        self.sbutton1 = customtki.CTkButton(self.sidebar_frame, height=40, width=120, text="Czas", command=lambda: self.change_item_list("czas"))
         self.sbutton1.grid(row=3, columnspan=2)
         
-        self.sbutton1 = customtki.CTkButton(self.sidebar_frame, height=40, width=120, text="Pole Powierzchni")
+        self.sbutton1 = customtki.CTkButton(self.sidebar_frame, height=40, width=120, text="Pole Powierzchni", command=lambda: self.change_item_list("pole powierzchni"))
         self.sbutton1.grid(row=4, columnspan=2)
         
-        self.sbutton1 = customtki.CTkButton(self.sidebar_frame, height=40, width=120, text="Objętość")
+        self.sbutton1 = customtki.CTkButton(self.sidebar_frame, height=40, width=120, text="Objętość", command=lambda: self.change_item_list("objętość"))
         self.sbutton1.grid(row=5, columnspan=2)
         
-        self.sbutton1 = customtki.CTkButton(self.sidebar_frame, height=40, width=120, text="Temperatura")
+        self.sbutton1 = customtki.CTkButton(self.sidebar_frame, height=40, width=120, text="Temperatura", command=lambda: self.change_item_list("temperatura"))
         self.sbutton1.grid(row=6, columnspan=2)
         
-        self.sbutton1 = customtki.CTkButton(self.sidebar_frame, height=40, width=120, text="Ciśnienie")
+        self.sbutton1 = customtki.CTkButton(self.sidebar_frame, height=40, width=120, text="Ciśnienie", command=lambda: self.change_item_list("ciśnienie"))
         self.sbutton1.grid(row=7, columnspan=2)
         
-        self.sbutton1 = customtki.CTkButton(self.sidebar_frame, height=40, width=120, text="Prędkość")
+        self.sbutton1 = customtki.CTkButton(self.sidebar_frame, height=40, width=120, text="Prędkość", command=lambda: self.change_item_list("prędkość"))
         self.sbutton1.grid(row=8, columnspan=2)
         
-        self.sbutton1 = customtki.CTkButton(self.sidebar_frame, height=40, width=120, text="Pojemność Pamięci")
+        self.sbutton1 = customtki.CTkButton(self.sidebar_frame, height=40, width=120, text="Pojemność Pamięci", command=lambda: self.change_item_list("pojemność pamięci"))
         self.sbutton1.grid(row=9, columnspan=2)
 
 
@@ -75,19 +76,34 @@ class App(customtki.CTk):
         self.entry.grid(row=2, column=1, padx=5, pady=0)
 
         # Pole wyjściowe
-        self.output = customtki.CTkLabel(self, height=55, width=220, text="Placeholder", font=customtki.CTkFont(size=20), fg_color="gray29", corner_radius=8)
+        self.output = customtki.CTkLabel(self, height=55, width=220, text="Bagno", font=customtki.CTkFont(size=20), fg_color="gray29", corner_radius=8)
         self.output.grid(row=2, column=3, padx=5, pady=0)
 
-        # Lista jednostek wejścia
-        self.listain = customtki.CTkComboBox(self, width=150,
-                                             values=["ocpja1", "opcja2", "opcja3"])
-        self.listain.grid(row=3, column=1, padx=0, pady=10)
+        # Podstawowa wartości dla listy jednostek
+        self.category = "dlugosc"
+        self.item_list = logika.fetch_items(self.category)
 
-        # Lista jednostek wyjścia
-        self.listain = customtki.CTkComboBox(self, width=150,
-                                             values=["ocpja1", "opcja2", "opcja3"])
-        self.listain.grid(row=3, column=3, padx=0, pady=10)
+        # Listy jednostek wejścia i wyjścia
+        self.lista_in = customtki.CTkComboBox(self, width=150, values= self.item_list)
+        self.lista_in.grid(row=3, column=1, padx=0, pady=10)
+
+        self.lista_out = customtki.CTkComboBox(self, width=150, values= self.item_list)
+        self.lista_out.grid(row=3, column=3, padx=0, pady=10)
         
         # Przycisk do konwersjii
-        self.button = customtki.CTkButton(self, height=40, width=180, text="Konwertuj")
+        self.button = customtki.CTkButton(self, height=40, width=180, text="Konwertuj", command=lambda: self.conversion_logic())
         self.button.grid(row=3, column=1, columnspan=3)
+    
+    # Funkcja do ktorej odwołują się przyciski, służy do zmiany listy przedmiotow uzywanej w drop down listach wyboru jednostek
+    def change_item_list(self, category):
+        item_list = logika.fetch_items(category)
+        self.lista_in.configure(values= item_list)
+        self.lista_out.configure(values= item_list)
+        self.lista_in.set(item_list[0])
+        self.lista_out.set(item_list[0])
+        self.category = category
+
+    def conversion_logic(self):
+        entry_value = self.entry.get()
+        converted_value = logika.convert(self.category, self.lista_in.get(), self.lista_out.get(), entry_value)
+        self.output.configure(text=converted_value)
